7daeb9c3768add91fa446f3c1d829b6efd0c0f4c
andrejarboe <andrejarboe@gmail.com>
2021-09-20 18:59:51 -0400

    finish

---
+++ js/app.js
@@ -0,0 +1,32 @@
+window.onload = function () {
+  let emailModal = document.getElementsByClassName('email-modal')[0];
+  let emailButton = document.getElementsByClassName('email-modal__button')[0];
+  let thankContainer = document.getElementsByClassName('email-thanks')[0];
+  let goShopButton = document.getElementsByClassName('email-modal__go-shop')[0];
+
+  let closeModal = () => {
+    emailModal.classList.remove('email-modal--visible');
+  };
+
+  let showThankMessage = () => {
+    thankContainer.classList.add('email-thanks--success');
+  };
+
+  let showModal = () => {
+    emailModal.classList.add('email-modal--visible');
+    thankContainer.classList.remove('email-thanks--success');
+  }
+
+  emailButton.addEventListener('click', () => {
+    showThankMessage();
+    console.log('clicked');
+  });
+
+  goShopButton.addEventListener('click', () => {
+    closeModal();
+    setTimeout(() => {
+        showModal();
+      }, 5000);
+  });
+
+};
